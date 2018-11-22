ecaffb9ed52e7e7a91d04e77d23c6c6b000f61ab
Robin Mirow <robinmirow@live.com>
2018-11-22 21:36:04 +0100

    Add simple text format for images

---
+++ simulator/src/asm.rs
@@ -0,0 +1,38 @@
+use byteorder::{ByteOrder, LittleEndian};
+use memory::Word;
+use std::io::{self, BufRead, Read, Write};
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+pub enum AsmError {
+    Io(io::Error),
+    InvalidWord(ParseIntError),
+}
+
+/// Converts a simple text file into a binary image that can be loaded by the vm.
+/// Each line is either empty, start with a `#` and is ignored, or contains a word
+/// formatted in binary. Whitespace is ignored.
+pub fn assemble<R, W>(src: R, mut dst: W) -> Result<(), AsmError>
+where
+    R: Read + BufRead,
+    W: Write,
+{
+    for line in src.lines() {
+        let line = line.map_err(AsmError::Io)?;
+
+        // strip line comments
+        let line = &line[..line.find("#").unwrap_or(line.len())];
+        let line = line.trim().replace(char::is_whitespace, "");
+
+        if line.is_empty() {
+            continue;
+        }
+
+        let word = Word::from_str_radix(&line, 2).map_err(AsmError::InvalidWord)?;
+        let mut bytes = [0; 4];
+        LittleEndian::write_u32(&mut bytes, word);
+        dst.write_all(&bytes).map_err(AsmError::Io)?;
+    }
+
+    Ok(())
+}
--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -5,15 +5,17 @@ extern crate structopt;
 extern crate strum;
 extern crate strum_macros;
 
+mod asm;
 mod memory;
 mod simulation;
 mod vm;
 
+use asm::AsmError;
 use memory::{Memory, Word, WORD_HEX_FMT_WIDTH};
 use simulation::Simulation;
 use std::fmt::{self, Display};
-use std::fs;
-use std::io;
+use std::fs::{self, File};
+use std::io::{self, BufReader};
 use std::path::PathBuf;
 use std::process;
 use structopt::StructOpt;
@@ -63,27 +65,57 @@ pub enum ErrorKind {
     UninitializedMemoryAccess(Word),
 }
 
+#[derive(Debug)]
+enum CliError {
+    Io(io::Error),
+    Asm(AsmError),
+}
+
+impl From<io::Error> for CliError {
+    fn from(err: io::Error) -> CliError {
+        CliError::Io(err)
+    }
+}
+
+impl From<AsmError> for CliError {
+    fn from(err: AsmError) -> CliError {
+        CliError::Asm(err)
+    }
+}
+
 #[derive(StructOpt)]
 struct Args {
     #[structopt(parse(from_os_str))]
     /// The image that is loaded into memory before startup. Addressing
     /// starts at 0x00000000
     image: PathBuf,
+    #[structopt(short = "c", long = "convert-from-text")]
+    /// Load the image from a text instead of a binary file. The file
+    /// must contain the binary representation of words, separated by newlines
+    convert_from_text: bool,
 }
 
 fn main() {
     match run(Args::from_args()) {
         Ok(_) => process::exit(0),
         Err(why) => {
-            eprintln!("error: {}", why);
+            // TODO: add Display impl
+            eprintln!("error: {:?}", why);
             process::exit(1);
         }
     }
 }
 
-fn run(args: Args) -> Result<(), io::Error> {
+fn run(args: Args) -> Result<(), CliError> {
+    let img = if args.convert_from_text {
+        let mut buf = Vec::new();
+        asm::assemble(BufReader::new(File::open(args.image)?), &mut buf)?;
+        buf
+    } else {
+        fs::read(args.image)?
+    };
+
     let mut mem = Memory::new();
-    let img = fs::read(args.image)?;
     mem.store(0, &img);
 
     let simulation = Simulation::new(
