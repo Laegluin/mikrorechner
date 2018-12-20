3656865ce7070b2d79b59e615b9941be77e2e5d3
MzudemO <m.c.lahann@googlemail.com>
2018-12-20 15:30:55 +0100

    fixed byte ordering, updated test case

--- Hardware/test_data_memory.vhd
+++ Hardware/test_data_memory.vhd
@@ -7,22 +7,46 @@ entity test_memory is
 end test_memory;
 
 architecture test_mem of test_memory is
-    component data_memory
-    port
-    (
-	clk, rst: in std_logic;
-        mem_address: in unsigned(bit_Width-1 downto 0); --assuming 32Bit address
-        mem_write_data: in unsigned(bit_Width-1 downto 0);
-        mem_read_on, mem_write_on: in std_logic;
-        mem_out: out unsigned(bit_Width-1 downto 0)
-    );
-    end component;
+	
+	component data_memory
+	port
+	(
+		clk, rst: in std_logic;
+    	mem_address: in unsigned(bit_Width-1 downto 0); --assuming 32Bit address
+    	mem_offset: in unsigned(bit_Width-1 downto 0);
+    	mem_write_data: in unsigned(bit_Width-1 downto 0);
+    	mem_rw_en: in unsigned(1 downto 0);
+    	mem_out: out unsigned(bit_Width-1 downto 0);
+
+    	pc_enable_in : in std_logic;
+    	pc_write_enable_in : in std_logic;
+    	C_in : in unsigned(bit_Width-1 downto 0);
+    	wb_control_in : in unsigned(1 downto 0);
+    	reg_imm_in : in unsigned(bit_Width-1 downto 0);
+    	c_address_in : in unsigned(adr_Width-1 downto 0);
+
+    	c_address_out : out unsigned(adr_Width-1 downto 0);
+    	jump_in  : in unsigned(bit_Width-1 downto 0);
+    	jump_out : out unsigned(bit_Width-1 downto 0);
+    	reg_imm_out : out unsigned(bit_Width-1 downto 0);
+    	wb_control_out : out unsigned(1 downto 0);
+    	C_out : out unsigned(bit_Width-1 downto 0);
+    	pc_write_enable_out : out std_logic;
+    	pc_enable_out : out std_logic
+	);
+	end component;
 
     signal clk, rst: std_logic;
     signal mem_address: unsigned(bit_Width-1 downto 0);
+	signal mem_offset: unsigned(bit_Width-1 downto 0);
+	signal mem_rw_en: unsigned(1 downto 0);
     signal mem_write_data: unsigned(bit_Width-1 downto 0);
-    signal mem_read_on, mem_write_on: std_logic;
     signal mem_out: unsigned(bit_Width-1 downto 0);
+	
+	signal pc_enable_in, pc_write_enable_in, pc_enable_out, pc_write_enable_out: std_logic;
+	signal wb_control_in, wb_control_out: unsigned(1 downto 0);
+	signal C_in, C_out, reg_imm_in, reg_imm_out, jump_in, jump_out: unsigned(bit_Width-1 downto 0);
+	signal c_address_in, c_address_out: unsigned(adr_Width-1 downto 0);
 
 
     begin
@@ -31,10 +55,26 @@ architecture test_mem of test_memory is
 	    clk => clk,
 	    rst => rst,
 	    mem_address => mem_address,
-	    mem_write_data => mem_write_data,
-	    mem_read_on => mem_read_on,
-	    mem_write_on => mem_write_on,
-	    mem_out => mem_out	
+    	mem_offset => mem_offset,
+    	mem_write_data => mem_write_data,
+    	mem_rw_en => mem_rw_en,
+    	mem_out => mem_out,
+
+    	pc_enable_in => pc_enable_in,
+    	pc_write_enable_in => pc_write_enable_in,
+    	C_in => C_in,
+    	wb_control_in => wb_control_in,
+    	reg_imm_in => reg_imm_in,
+    	c_address_in => c_address_in,
+
+    	c_address_out => c_address_out,
+    	jump_in  => jump_in,
+    	jump_out => jump_out,
+    	reg_imm_out => reg_imm_out,
+    	wb_control_out => wb_control_out,
+    	C_out => C_out,
+    	pc_write_enable_out => pc_write_enable_out,
+    	pc_enable_out => pc_enable_out
 	);
 
 	process
@@ -47,47 +87,47 @@ architecture test_mem of test_memory is
 
 	process
 	begin
-	    mem_read_on <= '1';
+	    mem_rw_en <= "10";
+		mem_offset <= to_unsigned(0, bit_Width);
 	    mem_address <= to_unsigned(0, bit_Width);
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(1, bit_Width);
-	    wait for 40 ns;
-
-	    mem_address <= to_unsigned(2, bit_Width);
-	    wait for 40 ns;
-
-	    mem_address <= to_unsigned(3, bit_Width);
-	    wait for 40 ns;
-
 	    mem_address <= to_unsigned(4, bit_Width);
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(5, bit_Width);
+	    mem_address <= to_unsigned(8, bit_Width);
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(6, bit_Width);
+	    mem_address <= to_unsigned(12, bit_Width);
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(7, bit_Width);
+	    mem_address <= to_unsigned(16, bit_Width);
 	    wait for 40 ns;
 
-	    mem_read_on <= '0';
+	    mem_address <= to_unsigned(20, bit_Width);
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(4, bit_Width);
+	    mem_address <= to_unsigned(24, bit_Width);
+	    wait for 40 ns;
+
+	    mem_address <= to_unsigned(28, bit_Width);
+	    wait for 40 ns;
+
+	    mem_rw_en <= "00";
+	    wait for 40 ns;
+
+	    mem_address <= to_unsigned(16, bit_Width);
 	    mem_write_data <= to_unsigned(9, bit_Width);
-	    mem_write_on <= '1';
+	    mem_rw_en <= "01";
 	    wait for 20 ns;
 
-	    mem_write_on <= '0';
-	    mem_read_on <= '1';
+	    mem_rw_en <= "10";
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(5, bit_Width);
+	    mem_address <= to_unsigned(20, bit_Width);
 	    wait for 40 ns;
 
-	    mem_address <= to_unsigned(6, bit_Width);
+	    mem_address <= to_unsigned(24, bit_Width);
 	    wait for 40 ns;
 
 	    rst <= '1';
