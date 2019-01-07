80906d4447e557cf34fc0154d3833ff17cc9765b
Robin Mirow <robinmirow@live.com>
2019-01-07 13:08:27 +0100

    Fix lookup with multiple scopes

--- cmlc/src/typecheck/scope_map.rs
+++ cmlc/src/typecheck/scope_map.rs
@@ -4,7 +4,11 @@ use std::hash::Hash;
 // the impl is very naive and could be made a lot more efficient
 
 /// A map with hierarchical, scoped keys.
-pub struct ScopeMap<K, V> {
+#[derive(Debug)]
+pub struct ScopeMap<K, V>
+where
+    K: Hash + Eq,
+{
     scopes: Vec<FnvHashMap<K, Level<K, V>>>,
 }
 
@@ -31,7 +35,9 @@ where
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        self.current_scope_mut()
+        let current_scope_idx = self.current_scope_idx();
+
+        self.scopes[current_scope_idx]
             .entry(key)
             .or_insert(Level {
                 value: None,
@@ -42,14 +48,15 @@ where
     }
 
     pub fn path_insert(&mut self, path: impl IntoIterator<Item = K>, value: V) -> Option<V> {
+        let current_scope_idx = self.current_scope_idx();
+
         let mut path: Vec<_> = path.into_iter().collect();
         let last_key = path.pop()?;
 
         let mut level = self;
 
         for key in path {
-            level = level
-                .current_scope_mut()
+            level = level.scopes[current_scope_idx]
                 .entry(key)
                 .or_insert(Level {
                     value: None,
@@ -63,9 +70,17 @@ where
     }
 
     pub fn get(&self, key: &K) -> Option<&V> {
-        self.current_scope()
-            .get(key)
-            .and_then(|level| level.value.as_ref())
+        for scope in self.scope_indices() {
+            let value = self.scopes[scope]
+                .get(key)
+                .and_then(|level| level.value.as_ref());
+
+            if let Some(value) = value {
+                return Some(value);
+            }
+        }
+
+        None
     }
 
     pub fn path_get<'a>(&self, path: impl IntoIterator<Item = &'a K>) -> Option<&V>
@@ -75,32 +90,42 @@ where
         let mut path: Vec<_> = path.into_iter().collect();
         let last_key = path.pop()?;
 
-        let mut level = self;
+        for scope in self.scope_indices() {
+            let mut level = self;
 
-        for key in path {
-            level = level.current_scope().get(key)?.next.as_ref()?;
+            for key in &path {
+                let maybe_level = level.scopes[scope]
+                    .get(key)
+                    .and_then(|level| level.next.as_ref());
+
+                level = match maybe_level {
+                    Some(level) => level,
+                    None => continue,
+                }
+            }
+
+            if let Some(value) = level.get(last_key) {
+                return Some(value);
+            }
         }
 
-        level.get(last_key)
+        None
     }
 
-    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
-        self.current_scope_mut()
-            .get_mut(key)
-            .and_then(|level| level.value.as_mut())
+    fn current_scope_idx(&self) -> usize {
+        self.scopes.len() - 1
     }
 
-    fn current_scope(&self) -> &FnvHashMap<K, Level<K, V>> {
-        &self.scopes[self.scopes.len() - 1]
-    }
-
-    fn current_scope_mut(&mut self) -> &mut FnvHashMap<K, Level<K, V>> {
-        let current_scope = self.scopes.len() - 1;
-        &mut self.scopes[current_scope]
+    fn scope_indices(&self) -> impl Iterator<Item = usize> {
+        (0..self.scopes.len()).rev()
     }
 }
 
-struct Level<K, V> {
+#[derive(Debug)]
+struct Level<K, V>
+where
+    K: Hash + Eq,
+{
     value: Option<V>,
     next: Option<ScopeMap<K, V>>,
 }
@@ -117,12 +142,18 @@ mod test {
         assert!(map.path_insert(vec![], true).is_none());
         assert!(map.path_get(vec![]).is_none());
 
+        map.enter_scope();
+        assert!(map.path_get(vec![]).is_none());
+
         // one element only
         let mut map = ScopeMap::<&str, bool>::new();
 
         assert_eq!(map.path_get(&vec!["a"]), None);
         assert_eq!(map.path_insert(vec!["a"], true), None);
         assert_eq!(map.path_insert(vec!["a"], true), Some(true));
+        assert_eq!(map.path_get(&vec!["a"]), Some(&true));
+
+        map.enter_scope();
         assert_eq!(map.path_get(&vec!["a"]), Some(&true));
 
         // long path
@@ -135,9 +166,31 @@ mod test {
         assert_eq!(map.path_get(&vec!["a"]), Some(&true));
         assert_eq!(map.path_get(&vec!["a", "b"]), None);
 
+        map.enter_scope();
+        assert_eq!(map.path_get(&vec!["a"]), Some(&true));
+        assert_eq!(map.path_get(&vec!["a", "b"]), None);
+
         assert_eq!(map.path_insert(vec!["a", "b"], false), None);
         assert_eq!(map.path_insert(vec!["a", "b"], false), Some(false));
         assert_eq!(map.path_get(&vec!["a"]), Some(&true));
         assert_eq!(map.path_get(&vec!["a", "b"]), Some(&false));
+
+        map.enter_scope();
+        assert_eq!(map.path_get(&vec!["a"]), Some(&true));
+        assert_eq!(map.path_get(&vec!["a", "b"]), Some(&false));
+    }
+
+    #[test]
+    fn insert_and_get() {
+        let mut map = ScopeMap::<&str, bool>::new();
+
+        assert_eq!(map.get(&"key"), None);
+        assert_eq!(map.insert("key", true), None);
+        assert_eq!(map.get(&"key"), Some(&true));
+        assert_eq!(map.insert("key", false), Some(true));
+        assert_eq!(map.get(&"key"), Some(&false));
+
+        map.enter_scope();
+        assert_eq!(map.get(&"key"), Some(&false));
     }
 }
