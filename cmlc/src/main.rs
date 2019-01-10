4cd8a8c091cab059df149427d257697ea640f4b6
Robin Mirow <robinmirow@live.com>
2019-01-10 21:25:01 +0100

    Take file as command line arg

--- cmlc/src/main.rs
+++ cmlc/src/main.rs
@@ -9,15 +9,24 @@ use crate::span::Spanned;
 use codespan::{CodeMap, FileMap, FileName};
 use codespan_reporting::termcolor::{ColorChoice, StandardStream};
 use codespan_reporting::{self, Diagnostic, Label, LabelStyle, Severity};
+use std::fs;
 use std::io;
 use std::path::PathBuf;
 use std::process;
+use structopt::StructOpt;
 
 #[derive(Debug)]
 enum Error {
     Lex(lexer::LexError),
     Parse(parser::ParseError),
     Type(typecheck::TypeError),
+}
+
+#[derive(StructOpt)]
+struct Args {
+    /// The source file to compile
+    #[structopt(parse(from_os_str))]
+    src: PathBuf,
 }
 
 fn main() {
@@ -32,12 +41,11 @@ fn main() {
 }
 
 fn run() -> Result<bool, io::Error> {
-    let mut code_map = CodeMap::new();
+    let args = Args::from_args();
 
-    let file_map = code_map.add_filemap(
-        FileName::Real(PathBuf::from("syntax.cml")),
-        include_str!("../tests/syntax.cml").to_owned(),
-    );
+    let mut code_map = CodeMap::new();
+    let src = fs::read_to_string(&args.src)?;
+    let file_map = code_map.add_filemap(FileName::Real(args.src), src);
 
     match compile(&file_map) {
         Ok(_) => Ok(true),
